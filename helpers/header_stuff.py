d895192b3ec3c99e2eab3eb163b43b5e2e904a03
SickAndTired <78124142+S1ckAndTired@users.noreply.github.com>
2023-06-21 10:18:47 -0300

    Update header_stuff.py
    New dict from custom headers and new list for api output

--- helpers/header_stuff.py
+++ helpers/header_stuff.py
@@ -1,5 +1,5 @@
-mind_headers = ["X-Frame-Options", "X-Content-Type-Options", "Referrer-Policy", "Strict-Transport-Security", "Content-Security-Policy", "Permissions-Policy", "X-XSS-Protection", "Access-Control-Allow-Origin"]
-
+mind_headers_html = ["X-Frame-Options", "X-Content-Type-Options", "Referrer-Policy", "Strict-Transport-Security", "Content-Security-Policy", "Permissions-Policy", "X-XSS-Protection", "Access-Control-Allow-Origin"]
+mind_headers_api = ["Cache-Control", "Content-Security-Policy", "Content-Type", "Strict-Transport-Security", "X-Content-Type-Options", "X-Frame-Options"]
 disclosure = ["Server", "X-Powered-By", "X-AspNet-Version", "X-AspNetMvc-Version"]
 
 exposed = {}
@@ -7,3 +7,4 @@ missing = []
 info_disc = []
 casual_headers = []
 on_the_fly = {}
+custom_headers = {}
