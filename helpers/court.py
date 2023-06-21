9d3b245b597b532a72efc01386faa065a14e9522
SickAndTired <78124142+S1ckAndTired@users.noreply.github.com>
2023-06-21 12:23:33 -0300

    Update court.py

--- helpers/court.py
+++ helpers/court.py
@@ -7,7 +7,7 @@ from helpers.out import *
 
 
 
-def judger(headers, Application_Programming_Interface, current_headers, target, url, wordlist):
+def judger(headers, api, current_headers, target, url, wordlist):
     if wordlist is not None:
         f = open("output-headers.txt", "a")
         f.write("[Info Disclosure]" + "\n")
@@ -22,8 +22,8 @@ def judger(headers, Application_Programming_Interface, current_headers, target, 
                     f.close()
                 else:
                     pass  
-        if Application_Programming_Interface is not None:
-            for must_have in mind_headers_Application_Programming_Interface:
+        if api is not None:
+            for must_have in mind_headers_api:
                 if must_have not in headers:
                     missing.append(must_have)
                 else:
@@ -47,7 +47,7 @@ def judger(headers, Application_Programming_Interface, current_headers, target, 
             for header in headers:
                 if information in header:
                     print(f"[*] - {header}:",headers[f"{header}"])    
-        if Application_Programming_Interface is True:
+        if api is True:
             for must_have in mind_headers_api:
                 if must_have not in headers:
                     missing.append(must_have)
@@ -56,7 +56,7 @@ def judger(headers, Application_Programming_Interface, current_headers, target, 
             for header in headers:
                     casual_headers.append(header)
             out(headers, current_headers, wordlist)
-        elif Application_Programming_Interface is False:
+        elif api is False:
             for must_have in mind_headers_html:
                 if must_have not in headers:
                     missing.append(must_have)
