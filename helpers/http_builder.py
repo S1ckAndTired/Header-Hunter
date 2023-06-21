93199791b53b99e11da6f508be9f1ed6005a4b29
SickAndTired <78124142+S1ckAndTired@users.noreply.github.com>
2023-06-21 10:13:47 -0300

    Update http_builder.py
    'Origin' hardcoded is gone. Now you can set the header on cmd.

--- helpers/http_builder.py
+++ helpers/http_builder.py
@@ -2,46 +2,19 @@
 
 from helpers.requester import requester
 
-def http_builder(targett, current_headers, origin, proxy, wordlist):
-    if origin:
-        if targett:
-            try:
-                if not targett.startswith("https://") or not origin.startswith("https://"):
-                    try:
-                        targett = (f"https://{targett.split('//')[1]}")
-                        if origin is not None:
-                            if origin.startswith("http://"): #or not 
-                                origin = (f"https://{origin.split('//')[1]}")
-                            elif origin.startswith("https://"):
-                                pass
-                            else:
-                                origin = (f"https://{origin}")
-                        requester(targett, origin, current_headers, proxy, wordlist)
-                    except:
-                        targett = (f"https://{targett}")
-                        if origin is not None:
-                            if origin.startswith("http://"): #or not 
-                                origin = (f"https://{origin.split('//')[1]}")
-                            elif origin.startswith("https://"):
-                                pass
-                            else:
-                                origin = (f"https://{origin}") 
-                        requester(targett, origin, current_headers, proxy, wordlist)
-                else:
-                    requester(targett, origin, current_headers, proxy, wordlist)
-            except AttributeError:
-                pass
+def http_builder(targett,  header, Application_Programming_Interface, current_headers, proxy, wordlist):
+    if targett:
+        try:
+            if not targett.startswith("https://"):
+                try:
+                    targett = (f"https://{targett.split('//')[1]}")
+                    requester(targett,  header, Application_Programming_Interface, current_headers, proxy, wordlist)
+                except:
+                    targett = (f"https://{targett}")
+                    requester(targett,  header, Application_Programming_Interface, current_headers, proxy, wordlist)
+            else:
+                requester(targett,  header, Application_Programming_Interface, current_headers, proxy, wordlist)
+        except AttributeError:
+            pass
     else:
-        if targett:
-            try:
-                if not targett.startswith("https://"):
-                    try:
-                        targett = (f"https://{targett.split('//')[1]}")
-                        requester(targett, origin, current_headers, proxy, wordlist)
-                    except: 
-                        targett = (f"https://{targett}")
-                        requester(targett, origin, current_headers, proxy, wordlist)
-                else:
-                    requester(targett, origin, current_headers, proxy, wordlist)
-            except AttributeError:
-                pass
+        pass
