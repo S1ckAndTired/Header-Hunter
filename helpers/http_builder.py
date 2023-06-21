782bfbf8b46a6709084880caaec107443796362b
SickAndTired <78124142+S1ckAndTired@users.noreply.github.com>
2023-06-21 12:22:57 -0300

    Update http_builder.py

--- helpers/http_builder.py
+++ helpers/http_builder.py
@@ -2,18 +2,18 @@
 
 from helpers.requester import requester
 
-def http_builder(targett,  header, Application_Programming_Interface, current_headers, proxy, wordlist):
+def http_builder(targett,  header, api, current_headers, proxy, wordlist):
     if targett:
         try:
             if not targett.startswith("https://"):
                 try:
                     targett = (f"https://{targett.split('//')[1]}")
-                    requester(targett,  header, Application_Programming_Interface, current_headers, proxy, wordlist)
+                    requester(targett,  header, api, current_headers, proxy, wordlist)
                 except:
                     targett = (f"https://{targett}")
-                    requester(targett,  header, Application_Programming_Interface, current_headers, proxy, wordlist)
+                    requester(targett,  header, api, current_headers, proxy, wordlist)
             else:
-                requester(targett,  header, Application_Programming_Interface, current_headers, proxy, wordlist)
+                requester(targett,  header, api, current_headers, proxy, wordlist)
         except AttributeError:
             pass
     else:
