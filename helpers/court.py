18cb8eed07ecf46a08f089f7321d3c54c7e4e371
SickAndTired <78124142+S1ckAndTired@users.noreply.github.com>
2023-06-21 10:16:45 -0300

    Update court.py
    Custom output based whether api flag is set or not

--- helpers/court.py
+++ helpers/court.py
@@ -2,11 +2,12 @@
 
 
 from helpers.header_stuff import *
+from colorama import Fore
 from helpers.out import *
 
 
 
-def judger(headers, current_headers, target, url, wordlist):
+def judger(headers, Application_Programming_Interface, current_headers, target, url, wordlist):
     if wordlist is not None:
         f = open("output-headers.txt", "a")
         f.write("[Info Disclosure]" + "\n")
@@ -21,28 +22,46 @@ def judger(headers, current_headers, target, url, wordlist):
                     f.close()
                 else:
                     pass  
-        for must_have in mind_headers:
-            if must_have not in headers:
-                missing.append(must_have)
-            else:
-                pass
-        for header in headers:
-                casual_headers.append(header)
-        out(headers, current_headers, wordlist)
+        if Application_Programming_Interface is not None:
+            for must_have in mind_headers_Application_Programming_Interface:
+                if must_have not in headers:
+                    missing.append(must_have)
+                else:
+                    pass
+            for header in headers:
+                    casual_headers.append(header)
+            out(headers, current_headers, wordlist)
+        else:
+            for must_have in mind_headers_html:
+                if must_have not in headers:
+                    missing.append(must_have)
+                else:
+                    pass
+            for header in headers:
+                    casual_headers.append(header)
+            out(headers, current_headers, wordlist)
     elif wordlist is None:
         print("")
         print("[Info Disclosure]")
         for information in disclosure:
             for header in headers:
                 if information in header:
-                    print(f"[*] - {header}:",headers[f"{header}"])
+                    print(f"[*] - {header}:",headers[f"{header}"])    
+        if Application_Programming_Interface is True:
+            for must_have in mind_headers_api:
+                if must_have not in headers:
+                    missing.append(must_have)
                 else:
-                    pass    
-        for must_have in mind_headers:
-            if must_have not in headers:
-                missing.append(must_have)
-            else:
-                pass
-        for header in headers:
-                casual_headers.append(header)
-        out(headers, current_headers, wordlist)
+                    pass
+            for header in headers:
+                    casual_headers.append(header)
+            out(headers, current_headers, wordlist)
+        elif Application_Programming_Interface is False:
+            for must_have in mind_headers_html:
+                if must_have not in headers:
+                    missing.append(must_have)
+                else:
+                    pass
+            for header in headers:
+                    casual_headers.append(header)
+            out(headers, current_headers, wordlist)
