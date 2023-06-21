33ecf6b1b396788ee9cf3e93c28dc1263701f9ee
SickAndTired <78124142+S1ckAndTired@users.noreply.github.com>
2023-06-21 10:12:09 -0300

    Update HeaderHunter.py
    Added api option

--- HeaderHunter.py
+++ HeaderHunter.py
@@ -8,32 +8,32 @@ from helpers.requester import requester
 from helpers.http_builder import http_builder
 from helpers.header_stuff import on_the_fly
 
-
+blah = {}
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-ch","--cheaders", action="store_true", help="Fetch current headers")
-parser.add_argument("-o", "--origin", metavar="", help="Origin required for API")
-parser.add_argument("-x", "--proxy", metavar="", help="Proxy - http://IP:PORT")
+parser.add_argument("-H", "--header", metavar="", action="append", help="This is self explaining")
+parser.add_argument("-x", "--proxy", metavar="", help="Prxy - http://IP:PORT")
+parser.add_argument("-api", "--Application-Programming-Interface", dest="Application_Programming_Interface", action="store_true", help="Set it for Application_Programming_Interface")
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument("-t", "--target", metavar="")
 group.add_argument("-w", "--wordlist", metavar="", help="Provide list of targets")
 parser.add_argument(" ", nargs="*")
 args = parser.parse_args()
 target = args.target
-origin = args.origin
+header = args.header
 wordlist = args.wordlist
 current_headers = args.cheaders
 proxy = args.proxy
-
-
+Application_Programming_Interface = args.Application_Programming_Interface
 
 def start():
-    print("Powered By: BERGHEM - Smart Security Information")
+    print("\nPowered By: BERGHEM - Smart Information Security")
     if wordlist:
         f = open(wordlist, "r")
         for t in f:
             targett = t.strip()
-            on_the_fly[targett] = on_the_fly.get(targett, 0) + 1
+            on_the_fly[targett] = on_the_fly.get(targett,  header, 0) + 1
         
         f = open(wordlist, "r")
         bar = Bar (f" [+] Targets analysed",max=sum(on_the_fly.values()), fill="█")
@@ -41,15 +41,15 @@ def start():
             print("\r    Fetching headers", end=""),bar.next()
             targett = t.strip()
             if current_headers == True:
-                http_builder(targett, current_headers, origin, proxy, wordlist)
+                http_builder(targett, header, Application_Programming_Interface, current_headers, proxy, wordlist)
             elif current_headers == False:
-                http_builder(targett, current_headers, origin, proxy, wordlist)
+                http_builder(targett, header, Application_Programming_Interface, current_headers, proxy, wordlist)
         bar.finish()
     elif target:
         targett = target
         if current_headers == True:
-            http_builder(targett, current_headers, origin, proxy, wordlist)
+            http_builder(targett,  header, Application_Programming_Interface, current_headers, proxy, wordlist)
         elif current_headers == False:
-            http_builder(targett, current_headers, origin, proxy, wordlist)
+            http_builder(targett,  header, Application_Programming_Interface, current_headers, proxy, wordlist)
 
 start()
